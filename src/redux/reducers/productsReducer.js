9a8cb5a9b9e838936a11faaf158ab6f7ab9ebe73
galbeiroc <alcresgu_1989@hotmail.com>
2021-05-08 20:26:35 -0500

    #03-configure product reducer

---
+++ src/redux/reducers/productsReducer.js
@@ -0,0 +1,18 @@
+import { ActionsTypes } from "../constants/actions-types";
+
+const initialState = {
+  products: [{
+    id: 1,
+    title: 'Dipesh',
+    category: 'programmer'
+  }]
+}
+
+export const productReducer = (state = initialState, {type, payload}) => {
+  switch (type) {
+    case ActionsTypes.SET_PRODUCTS:
+      return state;
+    default:
+      return state;
+  }
+};
